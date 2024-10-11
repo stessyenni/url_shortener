6b78ab512df493c27407d930d60b96e12704d378
stessyenni <50053328+stessyenni@users.noreply.github.com>
2024-10-11 20:15:06 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -2,37 +2,39 @@
 //Create DB connection
 $host = 'localhost';
 $dbname = 'url_shortener';
-$port = '5433';
+$port = '5432';
 $username = 'postgres';
-$password = '';
+$password = 'aset';
 
 //Creating a PDO DB Connection
 try{
     $pdo = new
 PDO("pgsql:host=$host;port=$port;dbname=$dbname;", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to Database Successfully";
+    #echo "Connected to Database Successfully";
 } catch (PDOException $e){
-    die("Could not Connect to the database: " .
-    $e->getMessage());
+    die("Could not Connect to the database: " . $e->getMessage());
 }
 
 //Get short code from URL
-if(isset($_GET['url'])){
-    $surl=$_GET['url'];
+if(isset($_GET['code'])){
+    $surl=$_GET['code'];
 
     //Retrieve original URL from DB
-    $urlquery=$pdo->prepare("SELECT lurl FROM url_shortener WHERE surl = :short_code");
-    $urlquery->execute([':short_code' => $surl]);
-    $result=$urlquery->fetch(PDO::FETCH_ASSOC);
+    $urlquery = $pdo->prepare("SELECT lurl FROM urls WHERE surl = :short_code");
+    $urlquery->execute(['short_code' => $surl]);
+    $result = $urlquery->fetch(PDO::FETCH_ASSOC);
 
     if($result){
         //redirecting to original URL
-        header('Location:'.$result['lurl']);
-        exit();
+        $lurl = $result['lurl'];
+        header("Location: $lurl");
+        exit;
     }else{
         echo "Invalid URL";
     }
+} else {
+    echo "No short URL code provided!";
 
 }
 ?>
